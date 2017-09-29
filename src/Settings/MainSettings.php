c75016797d043b1be9f217a2cb0ecd12dcd2147a
fionamatters <fiona.matters@solarwinds.com>
2017-09-29 20:51:56 +0100

    Start settings

--- src/Settings/MainSettings.php
+++ src/Settings/MainSettings.php
@@ -1,13 +1,32 @@
 <?php
+
 namespace Matters\GeneticSim\Settings;
+
 use Solarwinds\Oauth2\Utilities\HelperFunctionWrapper;
 
-class MainSettings{
+class MainSettings
+{
+    const DSN_FORMAT = '%s:host=%s;dbname=%s;charset=UTF8';
     private $stdObj;
     public function __construct(HelperFunctionWrapper $helperFunctionWrapper)
     {
         $settingsFile = realpath(__DIR__).'/../../Settings/settings.json';
         $this->stdObj = json_decode($helperFunctionWrapper->fileGetContents($settingsFile));
+    }
+
+    public function getDsn()
+    {
+        return sprintf(self::DSN_FORMAT,  $this->stdObj->db->engine, $this->stdObj->db->host, $this->stdObj->db->database);
+    }
+
+    public function getDbUsername()
+    {
+        return $this->stdObj->db->username;
+    }
+
+    public function getDbPassword()
+    {
+        return $this->stdObj->db->password;
     }
 
 
--- test/Settings/MainSettingsTest.php
+++ test/Settings/MainSettingsTest.php
@@ -3,6 +3,7 @@ namespace Matters\GeneticSim\Test\Settings;
 
 use Matters\GeneticSim\Settings\MainSettings;
 use Matters\GeneticSim\Test\TestCase;
+use PDO;
 use Solarwinds\Oauth2\Utilities\HelperFunctionWrapper;
 
 class MainSettingsTest extends TestCase
@@ -10,5 +11,9 @@ class MainSettingsTest extends TestCase
 
     public function testOne(){
         $subject = new MainSettings(new HelperFunctionWrapper());
+        $db1 = new PDO('mysql:host=localhost;dbname=hounddog','root','');
+        $a = $subject->getDsn();
+        $db = new PDO($subject->getDsn(), $subject->getDbUsername(), $subject->getDbPassword());
+        $a = 1;
     }
 }
